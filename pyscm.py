cbd1ef9d7db6151b62ec6742dd36d9b1c119eb66
helei <heleifz@126.com>
2014-07-14 16:32:58 +0800

    metacircular evaluator; TODO : quotation, symbol

--- pyscm.py
+++ pyscm.py
@@ -21,21 +21,198 @@ def parse(code):
             ast.append([])
         elif p == ')':
             lst = ast.pop() 
-            # deal with quoting
             if not ast[-1] or ast[-1][-1] != "'":
                 ast[-1].append(lst)
+            # deal with quoting
             else:
                 ast[-1].pop()
                 ast[-1].append(['quote', lst])
         else:
             ast[-1].append(p)
     ast = ast[0]
+    ast.insert(0, 'begin')
     return ast
+
+# Convert nested list to pairs
+
+def pylist_to_pairs(lst):
+    if not isinstance(lst, list):
+        return lst
+    elif len(lst) == 0:
+        return []
+    else:
+        return [pylist_to_pairs(lst[0]), pylist_to_pairs(lst[1:])]
 
 # Predicators
 
+def is_number(exp):
+    try:
+        float(exp)
+        return True
+    except ValueError:
+        return False
 
-def evaluate(lst, env):
+def is_string(exp):
+    if exp[0] == exp[-1] == '"':
+        return True
+
+def is_primitive(exp):
+    if not isinstance(exp, basestring):
+        return False
+    if is_number(exp):
+        return True
+    elif is_string(exp):
+        return True
+    return False
+
+def is_tagged_list(exp, tag):
+    return isinstance(exp, list) and len(exp) > 0 and exp[0] == tag
+
+def is_variable(exp):
+    return not is_primitive(exp) and isinstance(exp, basestring)
+
+def is_lambda(exp):
+    return is_tagged_list(exp, 'lambda')
+
+def is_definition(exp):
+    return is_tagged_list(exp, 'define')
+
+def is_application(exp):
+    return isinstance(exp, list) and len(exp) > 0
+
+def is_sequence(exp):
+    return is_tagged_list(exp, 'begin')
+
+def is_quote(exp):
+    return is_tagged_list(exp, 'quote')
+
+# Primitive functions
+
+def display(args):
+    content = ""
+    for a in args:
+        content += str(a) + ' '
+    print content
+
+def add(args):
+    return reduce(lambda x,y:x+y, args, 0)
+
+def sub(args):
+    return reduce(lambda x,y:x-y, args)
+
+def mul(args):
+    return reduce(lambda x,y:x*y, args, 1)
+
+def div(args):
+    return reduce(lambda x,y:x/y, args)
+
+def cons(args):
+    return [args[0], args[1]]
+
+def cdr(args):
+    return args[0][1]
+
+def car(args):
+    return args[0][0]
+
+# Classes
+
+class Lambda(object):
+    def __init__(self, paras, body, parent_env):
+        self.__paras = paras
+        self.__body = body
+        self.__parent_env = parent_env
+
+    @staticmethod
+    def make(lst, env):
+        return Lambda(lst[1], lst[2:], env)
+
+    def apply(self, args):
+        env = dict(zip(self.__paras, args))
+        env['**parent**'] = self.__parent_env
+        return eval_sequence(self.__body, env)
+
+    def get_paras(self):
+        return self.__paras 
+    def get_body(self):
+        return self.__body
+    def get_parent_env(self):
+        return self.__parent_env
+
+class PrimitiveFunction(object):
+    def __init__(self, func):
+        self.__func = func
+
+    def apply(self, args):
+        return self.__func(args) 
+
+# Metacircular evaluator
+
+def eval_primitive(exp):
+    if is_number(exp):
+        return float(exp)
+    elif is_string(exp):
+        return exp.strip('"');
+
+def eval_variable(exp, env):
+    if exp in env:
+        return env[exp]
+    elif '**parent**' in env:
+        return eval_variable(exp, env['**parent**'])
+    else:
+        raise Exception('Undefine variable : ' + exp)
+
+def eval_definition(exp, env):
+    if is_variable(exp[1]):
+        env[exp[1]] = evaluate(exp[2], env)
+    else:
+        env[exp[1][0]] = Lambda(exp[1][1:], exp[2:], env)
+
+def eval_application(exp, env):
+    args = [evaluate(arg, env) for arg in exp]
+    return args[0].apply(args[1:])
+
+def eval_quote(exp):
+    return pylist_to_pairs(exp[1])
+
+def eval_sequence(exp, env):
+    result = None
+    for e in exp:
+        result = evaluate(e, env)
+    return result
+
+def evaluate(exp, env):
     """Evaluate parsed Scheme list in an environment"""
-    pass
+    if is_primitive(exp):
+        return eval_primitive(exp)
+    elif is_variable(exp):
+        return eval_variable(exp, env)
+    elif is_quote(exp):
+        return eval_quote(exp)
+    elif is_lambda(exp):
+        return Lambda.make(exp, env)
+    elif is_definition(exp):
+        return eval_definition(exp, env)
+    elif is_sequence(exp):
+        return eval_sequence(exp[1:], env)
+    # must be the last clause
+    elif is_application(exp):
+        return eval_application(exp, env)
+    else:
+        pass
+
+# Make base Environment
+
+def make_base():
+    env = {}
+    env['display'] = PrimitiveFunction(display)
+    env['cons'] = PrimitiveFunction(cons)
+    env['car'] = PrimitiveFunction(car)
+    env['cdr'] = PrimitiveFunction(cdr)
+    env['+'] = PrimitiveFunction(add)
+    env['-'] = PrimitiveFunction(sub)
+    env['*'] = PrimitiveFunction(mul)
+    env['/'] = PrimitiveFunction(div)
+    return env
+
 
--- tests.py
+++ tests.py
@@ -4,40 +4,40 @@ import pyscm
 class TestParse(unittest.TestCase):
 
     def test_single_list(self):
-        self.assertEqual([['+', '1', '2']],
+        self.assertEqual(['begin', ['+', '1', '2']],
             pyscm.parse('(+ 1 2)'))
-        self.assertEqual([['number?', 'x']],
+        self.assertEqual(['begin', ['number?', 'x']],
             pyscm.parse('(number? x)'))
-        self.assertEqual([['lambda', ['x'], ['+', 'x', '3']]],
+        self.assertEqual(['begin', ['lambda', ['x'], ['+', 'x', '3']]],
             pyscm.parse('(lambda (x) (+ x 3))'))
-        self.assertEqual([['define', 'a', '5']],
+        self.assertEqual(['begin', ['define', 'a', '5']],
             pyscm.parse('(define a 5)'))
-        self.assertEqual([['if', 'a', '#f', '1', '3']],
+        self.assertEqual(['begin', ['if', 'a', '#f', '1', '3']],
             pyscm.parse('(if a #f 1 3)'))
 
     def test_nested_list(self):
-        self.assertEqual([['+', ['*', 'a', 'b'], '4']],
+        self.assertEqual(['begin', ['+', ['*', 'a', 'b'], '4']],
             pyscm.parse('(+ (* a b) 4)'))
-        self.assertEqual([['+', ['*', 'a', ['/', '12.3', '4.3']], '4']],
+        self.assertEqual(['begin', ['+', ['*', 'a', ['/', '12.3', '4.3']], '4']],
             pyscm.parse('(+ (* a (/ 12.3 4.3)) 4)'))
 
     def test_sequence(self):
-        self.assertEqual([['+', '3', '4'], ['*', '1', '2']],
+        self.assertEqual(['begin', ['+', '3', '4'], ['*', '1', '2']],
             pyscm.parse('(+ 3 4) (* 1 2)'))
-        self.assertEqual([['+', '3', '4'], ['*', '1', '2']],
+        self.assertEqual(['begin', ['+', '3', '4'], ['*', '1', '2']],
             pyscm.parse('(+ 3 4) \n\n(* 1 2)'))
 
     def test_sequence_and_nested(self):
-        self.assertEqual([['+', ['a', '1', '2'], '4'], ['*', '1', '2']],
+        self.assertEqual(['begin', ['+', ['a', '1', '2'], '4'], ['*', '1', '2']],
             pyscm.parse('(+ (a 1 2) 4) (* 1 2)'))
 
     def test_quote(self):
-        self.assertEqual([['quote', ['1', '2']]], pyscm.parse("'(1 2)"))
-        self.assertEqual([['quote', ['define', 'a', '1']]],
+        self.assertEqual(['begin', ['quote', ['1', '2']]], pyscm.parse("'(1 2)"))
+        self.assertEqual(['begin', ['quote', ['define', 'a', '1']]],
             pyscm.parse("'(define a 1)"))
 
     def test_data_type(self):
-        self.assertEqual([['"hello"', '"world"', '#f', '#t', '1e4']],
+        self.assertEqual(['begin', ['"hello"', '"world"', '#f', '#t', '1e4']],
             pyscm.parse('("hello" "world" #f #t 1e4)'))
 
     def test_function(self):
@@ -46,12 +46,117 @@ class TestParse(unittest.TestCase):
             (if (pair? exp)
                 (eq? (car exp) tag)
                 false))"""
-        ast = [['define', ['tagged-list?', 'exp', 'tag'],
+        ast = ['begin', ['define', ['tagged-list?', 'exp', 'tag'],
             ['if', ['pair?', 'exp'],
             ['eq?', ['car', 'exp'], 'tag'],
             'false']]]
         self.assertEqual(ast, pyscm.parse(code))
-        
+
+class TestPredicators(unittest.TestCase):
+
+    def test_is_primitive(self):
+        self.assertTrue(pyscm.is_primitive('12'))
+        self.assertTrue(pyscm.is_primitive('12.2'))
+        self.assertTrue(pyscm.is_primitive('1.2e20'))
+        self.assertTrue(pyscm.is_primitive('"hello world"'))
+        self.assertFalse(pyscm.is_primitive(['define', 'a', '3']))
+        self.assertFalse(pyscm.is_primitive('"illgal string'))
+
+    def test_is_variable(self):
+        self.assertTrue(pyscm.is_variable('a'))
+        self.assertTrue(pyscm.is_variable('name_name'))
+        self.assertFalse(pyscm.is_variable('33'))
+        self.assertFalse(pyscm.is_variable('"foobar"'))
+        self.assertFalse(pyscm.is_variable(['+', '1', '2']))
+
+    def test_is_lambda(self):
+        self.assertTrue(pyscm.is_lambda(['lambda',
+            ['x'], ['+', '1', '2']]))
+        self.assertTrue(pyscm.is_lambda(['lambda', ['x', 'y'],
+            ['+', 'x', 'y']]))
+        self.assertFalse(pyscm.is_lambda(['define', 'x', '1']))
+        self.assertFalse(pyscm.is_lambda("var"))
+
+    def test_is_definition(self):
+        self.assertTrue(pyscm.is_definition(['define', 'x', '1']))
+        self.assertTrue(pyscm.is_definition(['define', ['x','y'],
+            ['+', 'x', 'y']]))
+
+    def test_is_sequence(self):
+        self.assertTrue(pyscm.is_sequence(['begin']))
+        self.assertTrue(pyscm.is_sequence(['begin', [], []]))
+        self.assertFalse(pyscm.is_sequence(['foo', [], []]))
+
+class TestLambda(unittest.TestCase):
+
+    def test_getter(self):
+        l = pyscm.Lambda(['a', 'b', 'c'], ['+', 'a', 'b'], {})
+        self.assertEqual(['a', 'b', 'c'], l.get_paras())
+        self.assertEqual(['+', 'a', 'b'], l.get_body())
+        self.assertEqual({}, l.get_parent_env())
+
+    def test_make(self):
+        l = pyscm.Lambda.make(['lambda', ['a', 'b', 'c'], ['+', 'a', 'b']], {})
+        self.assertEqual(['a', 'b', 'c'], l.get_paras())
+        self.assertEqual([['+', 'a', 'b']], l.get_body())
+        self.assertEqual({}, l.get_parent_env())
+
+
+class TestEvaludator(unittest.TestCase):
+
+    def test_primitive(self):
+        self.assertEqual(3, pyscm.eval_primitive('3'))
+        self.assertEqual(12.3, pyscm.eval_primitive('1.23e1'))
+        self.assertEqual("hello", pyscm.eval_primitive('"hello"'))
+
+    def test_variable(self):
+        self.assertEqual(3, pyscm.eval_variable('a', {'a':3}))
+        self.assertEqual(3, pyscm.eval_variable('a', {'**parent**':{'a':3}}))
+        self.assertRaises(Exception, pyscm.eval_variable, 'a', {})
+
+    def test_lambda(self):
+        l = pyscm.evaluate(['lambda', ['a', 'b', 'c'], ['+', 'a', 'b']], {})
+        self.assertTrue(isinstance(l, pyscm.Lambda))
+        self.assertEqual(['a', 'b', 'c'], l.get_paras())
+        self.assertEqual([['+', 'a', 'b']], l.get_body())
+        self.assertEqual({}, l.get_parent_env())
+
+    def test_definition(self):
+        env = {}
+        pyscm.evaluate(['define', 'a', '3'], env)
+        pyscm.evaluate(['define', 'foo', '"bar"'], env)
+        pyscm.evaluate(['define', ['func', 'p1', 'p2'],
+           ['+', 'p1', 'p2']], env)
+        self.assertEqual(3, env['a'])
+        self.assertEqual("bar", env['foo'])
+        l = env['func']
+        self.assertTrue(isinstance(l, pyscm.Lambda))
+        self.assertEqual(['p1', 'p2'], l.get_paras())
+        self.assertEqual([['+', 'p1', 'p2']], l.get_body())
+        self.assertEqual(env, l.get_parent_env())
+
+    def test_sequence(self):
+        env = {}
+        pyscm.evaluate(['begin', ['define', 'a', '3'],
+                                 ['define', 'foo', '"bar"'],
+                                 ['define', ['func', 'p1', 'p2'],
+                                    ['+', 'p1', 'p2']]], env)
+        self.assertEqual(3, env['a'])
+        self.assertEqual("bar", env['foo'])
+        l = env['func']
+        self.assertTrue(isinstance(l, pyscm.Lambda))
+        self.assertEqual(['p1', 'p2'], l.get_paras())
+        self.assertEqual([['+', 'p1', 'p2']], l.get_body())
+        self.assertEqual(env, l.get_parent_env())
+
+    def test_apply(self):
+        code = """
+        (define accu '(1 2 3))
+        (define x (cons 1 accu))
+        (display (+ (car (cdr x)) 3))
+        """
+        ast = pyscm.parse(code)
+        pyscm.evaluate(ast, pyscm.make_base())
 
 
 if __name__ == '__main__':
